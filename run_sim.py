9d8b319a95c0c34a9e60f594b55ce24916ceca4e
Corrosive <coltonbruni@Coltons-MacBook-Pro.local>
2023-03-03 12:19:41 -0600

    update run_sim to new data struct, send to supercomputer

--- run_sim.py
+++ run_sim.py
@@ -14,9 +14,11 @@ WVL = [220, 222]
 #WVL = [230]
 
 output_folder = "kappa"
+kappa = True
 
 keys = {}  # Create empty dictionary for keys
 
+set = Structure()
 for wvl in WVL:
     output_file = output_folder+"/fe3_"+str(wvl)+".json"
     Au_nk = np.loadtxt('Al_OMEL_mfp.nk')   #Al not GOLD!!!!!!!!!!!!!!!!!!!!!!!
@@ -84,7 +86,7 @@ for wvl in WVL:
             keys['n_2'] = np.interp(keys['vacuum_wavelength'], wl_Au_data, n_Au_real) + 1j*np.interp(keys['vacuum_wavelength'], wl_Au_data, n_Au_imag)
             keys['n_3'] = np.interp(keys['vacuum_wavelength'], wl_Aminoacid_data, n_Aminoacid_real) + 1j*np.interp(keys['vacuum_wavelength'], wl_Aminoacid_data, n_Aminoacid_imag)
             keys['kappa'] = np.interp(keys['vacuum_wavelength'], wl_Aminoacid_kappa, kappa_Aminoacid_real) + 1j*np.interp(keys['vacuum_wavelength'], wl_Aminoacid_kappa, kappa_Aminoacid_imag)
-            keys['sm_filename'] = '"'+'project_results/sm.jcm"'
+            #keys['sm_filename'] = '"'+'project_results/sm.jcm"'
             keys['azimuth'] = azi
             jcmwave.jcmt2jcm('boundary_conditions.jcmt', keys)
             jcmwave.jcmt2jcm('materials.jcmt', keys)
@@ -93,45 +95,68 @@ for wvl in WVL:
             jcmwave.jcmt2jcm('layout.jcmt', keys)
             jcmwave.solve('project.jcmp')
 		
+
+            # extract absorption data:
+            fileName = './project_results/absorption.jcm'
+            table = jcmwave.loadtable(fileName)
+            n2 = np.nonzero(table['DomainId'] == 2) # row number for Material Id 2 (Al Pillar)
+            n3 = np.nonzero(table['DomainId'] == 3) # row number for Material Id 3 (Al Film)
+            n4 = np.nonzero(table['DomainId'] == 4) # row number for Material Id 4 (Amino Acid)
+
+            # Power from plane wave
+            #p_in = (0.5)*(Eamp**2/Z1)*(((3*np.sqrt(3))/2)*((keys['pitch']*keys['uol'])**2)))*math.cos(math.radians(keys['AOI'])) # ((Eamp V/m)/ohms)*m^2 = Watts
+            p_in = 1.0 * (((3*np.sqrt(3))/2)*((keys['pitch']*keys['uol'])**2))*math.cos(math.radians(keys['AOI'])) # Eamp is not used because PowerScaling is being used in sources.jcmt
+
+            absR_pillar = np.real(table['ElectromagneticFieldAbsorption'][0][n2])/p_in
+            absL_pillar = np.real(table['ElectromagneticFieldAbsorption'][1][n2])/p_in
+         
+            absR_film = np.real(table['ElectromagneticFieldAbsorption'][0][n3])/p_in
+            absL_film = np.real(table['ElectromagneticFieldAbsorption'][1][n3])/p_in
+            
+            absR_amino = np.real(table['ElectromagneticFieldAbsorption'][0][n4])/p_in
+            absL_amino = np.real(table['ElectromagneticFieldAbsorption'][1][n4])/p_in
+                
+
             # Gather Reflected Fourier Modes (Z)
             filename_fourierModes_r = './project_results/fourier_modes_r.jcm';
             fourierModes_r = jcmwave.loadtable(filename_fourierModes_r,format='named')
             powerFlux_r = jcmwave.convert2powerflux(fourierModes_r)
 
             # Reflected flux in normal direction
-            P_s_t = np.sum(powerFlux_r['PowerFluxDensity'][0][:, 2]);
-            P_p_t = np.sum(powerFlux_r['PowerFluxDensity'][1][:, 2]); 
+            P_s_r = np.sum(powerFlux_r['PowerFluxDensity'][0][:, 2]);
+            P_p_r = np.sum(powerFlux_r['PowerFluxDensity'][1][:, 2]); 
 		
-            filename_MM = './project_results/sm.jcm'
-            table = jcmwave.loadtable(filename_MM)
-            row1 = [
-	        float(table['Mueller_xy11'][0]), 
-		float(table['Mueller_xy12'][0]),
-		float(table['Mueller_xy13'][0]),
-		float(table['Mueller_xy14'][0])]
-            row2 = [
-		float(table['Mueller_xy21'][0]),
-		float(table['Mueller_xy22'][0]),
-		float(table['Mueller_xy23'][0]),
-		float(table['Mueller_xy24'][0])]
-            row3 = [
-		float(table['Mueller_xy31'][0]),
-		float(table['Mueller_xy32'][0]),
-		float(table['Mueller_xy33'][0]),
-		float(table['Mueller_xy34'][0])]
-            row4 = [
-		float(table['Mueller_xy41'][0]),
-		float(table['Mueller_xy42'][0]),
-		float(table['Mueller_xy43'][0]),
-		float(table['Mueller_xy44'][0])]
-            mm = [row1, row2, row3, row4]
-            store[aoi][azi] = {}
-            store[aoi][azi]['mm'] = mm
+            #filename_MM = './project_results/sm.jcm'
+            #table = jcmwave.loadtable(filename_MM)
+            #row1 = [
+	        #float(table['Mueller_xy11'][0]), 
+		#float(table['Mueller_xy12'][0]),
+		#float(table['Mueller_xy13'][0]),
+		#float(table['Mueller_xy14'][0])]
+            #row2 = [
+		#float(table['Mueller_xy21'][0]),
+		#float(table['Mueller_xy22'][0]),
+		#float(table['Mueller_xy23'][0]),
+		#float(table['Mueller_xy24'][0])]
+            #row3 = [
+		#float(table['Mueller_xy31'][0]),
+		#float(table['Mueller_xy32'][0]),
+		#float(table['Mueller_xy33'][0]),
+		#float(table['Mueller_xy34'][0])]
+            #row4 = [
+		#float(table['Mueller_xy41'][0]),
+		#float(table['Mueller_xy42'][0]),
+		#float(table['Mueller_xy43'][0]),
+		#float(table['Mueller_xy44'][0])]
+            #mm = [row1, row2, row3, row4]
+            #store[aoi][azi] = {}
+            #store[aoi][azi]['mm'] = mm
 
-            #set.append(Tag(id_names, [azi, aoi, wvl, radius, pitch, height, kappa]), ["mm", "DI", "dMM"], [values.get("mm"), values.get("DI"), values.get("dMM")])
-            format_module.save_json(output_file,store)
+            id_names = ['azimuth','AOI','wvl','radius','pitch','height', 'kappa']
+            set.append(Tag(id_names, [azi, aoi, wvl, keys['radius'], keys['pitch'], keys['height'], kappa]), ["abs pillar", "abs film", "abs amino", "reflected flux"], [[absR_pillar, absL_pillar], [absR_film, absL_film], [absR_amino, absL_amino], [P_s_r, P_p_r]])
+            set.save_json(output_file)
 	    
         toc = time.time() # use time() not clock() on linux system  
         t = toc-tic
         print ("Total runtime for WVL: "+str(wvl)+" AOI: "+str(aoi)+" aziumth: "+str(azi)+" - %6.4f s" % t)
-       
+
