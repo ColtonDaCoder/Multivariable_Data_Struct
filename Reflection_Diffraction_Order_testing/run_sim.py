735e21c284348f9d163d89a6fb02af69ed7b9894
Colton Bruni <colton.bruni@gmail.com>
2023-04-06 12:33:05 -0500

    sending for diffraction order code writing in nvim

---
+++ Reflection_Diffraction_Order_testing/run_sim.py
@@ -0,0 +1,159 @@
+import numpy as np
+import math
+from data_format import Structure, Tag
+import format_module
+import json
+import jcmwave,time,imp,shutil,os 
+from optparse import OptionParser
+AOI = [i*4+48 for i in range(16)]
+AZI = [i for i in range(46)]
+
+
+#AOI = [20]
+#AZI = [34]
+
+WVL = [220, 222]
+
+output_folder = "kappa"
+kappa = True 
+
+keys = {}  # Create empty dictionary for keys
+
+set = Structure()
+for wvl in WVL:
+    output_file = output_folder+"/testing_ascii.json"
+    Al_nk = np.loadtxt('Al_OMEL_mfp.nk')   #Al not GOLD!!!!!!!!!!!!!!!!!!!!!!!
+    wl_Al_data = []; n_Al_real = []; n_Al_imag = []
+    for data in Al_nk:
+        wl_Al_data.append(data[0]*1e-9) # e-10 for [ang], e-9 for [nm], e-6 for [um]
+        n_Al_real.append(data[1])
+        n_Al_imag.append(data[2])
+
+    Aminoacid_nk = np.loadtxt('L-tyr_nx_kx.txt')
+    wl_Aminoacid_data = []; n_Aminoacid_real = []; n_Aminoacid_imag = []
+    for data in Aminoacid_nk:
+        wl_Aminoacid_data.append(data[0]*1e-9) # e-10 for [ang], e-9 for [nm], e-6 for [um]
+        n_Aminoacid_real.append(data[1])
+        n_Aminoacid_imag.append(data[2])
+        
+    Aminoacid_kappa = np.loadtxt('L-tyr_kappa.txt')
+    wl_Aminoacid_kappa = []; kappa_Aminoacid_real = []; kappa_Aminoacid_imag = []
+    for data in Aminoacid_kappa:
+        wl_Aminoacid_kappa.append(data[0]*1e-9) # e-10 for [ang], e-9 for [nm], e-6 for [um]
+        kappa_Aminoacid_real.append(data[1])
+        kappa_Aminoacid_imag.append(data[2])
+
+    dict = {}
+    format_module.save_json(output_file,dict)
+    for aoi in AOI:
+        parser = OptionParser()
+        parser.add_option("-t", "--threads",
+        action="store",type="int", dest="threads",
+        help="number of threads to use")
+        (options, args) = parser.parse_args()
+
+        jcmwave.set_num_threads(options.threads)
+        
+        keys = {} # Create empty dictionary for keys
+        dict[aoi] = {}
+        results = [] # Create empty array for results
+        tic = time.time() # use time() not clock() on linux system 
+        # Set simulation parameters
+        
+        keys = {
+            'AOI': aoi,
+            'radius': 50,
+            'vacuum_wavelength': wvl*1e-9,
+            'uol': 1e-9,
+            'display_triangulation' : 'no',
+            'boundary' : 'Periodic',
+            'info_level' : -1,
+            'fem_degree' : 3,
+            'n_refinement_steps' : 0, # Currently we get non-physical results if this is >0
+            'thickness' : 50,
+            'pitch' : 175, # pitch of square lattice (gammadion)
+            'z_radius' : 5, # radius of curvature of dimer in z plane
+            'z_radius_MSL' : 1, # maximum side length of z radius
+            'isKappa' : kappa
+            }
+
+        # material properties
+        keys['n_1'] = 1.00 # index refraction of Air
+        
+        store  = format_module.json_file(output_file).data
+        store[aoi] = {}
+ 
+        for azi in AZI:
+            #print('Wavelength : %3.2f nm' % (keys['vacuum_wavelength']*1e9))
+            keys['n_2'] = np.interp(keys['vacuum_wavelength'], wl_Al_data, n_Al_real) + 1j*np.interp(keys['vacuum_wavelength'], wl_Al_data, n_Al_imag)
+            keys['n_4'] = np.interp(keys['vacuum_wavelength'], wl_Aminoacid_data, n_Aminoacid_real) + 1j*np.interp(keys['vacuum_wavelength'], wl_Aminoacid_data, n_Aminoacid_imag)
+            keys['kappa'] = np.interp(keys['vacuum_wavelength'], wl_Aminoacid_kappa, kappa_Aminoacid_real) + 1j*np.interp(keys['vacuum_wavelength'], wl_Aminoacid_kappa, kappa_Aminoacid_imag)
+            keys['sm_filename'] = '"'+'project_results/sm.jcm"'
+            keys['azimuth'] = azi
+            jcmwave.jcmt2jcm('boundary_conditions.jcmt', keys)
+            jcmwave.jcmt2jcm('materials.jcmt', keys)
+            jcmwave.jcmt2jcm('project.jcmpt', keys)
+            jcmwave.jcmt2jcm('sources.jcmt', keys)
+            jcmwave.jcmt2jcm('layout.jcmt', keys)
+            jcmwave.solve('project.jcmp')
+		
+
+            # extract absorption data:
+            fileName = './project_results/absorption.jcm'
+            table = jcmwave.loadtable(fileName)
+            n1 = np.nonzero(table['DomainId'] == 1) # row number for Material Id 1 (Air)
+            n2 = np.nonzero(table['DomainId'] == 2) # row number for Material Id 2 (Al Pillar)
+            n3 = np.nonzero(table['DomainId'] == 3) # row number for Material Id 3 (Al Film)
+            n4 = np.nonzero(table['DomainId'] == 4) # row number for Material Id 4 (Amino Acid)
+
+            # Power from plane wave
+            #p_in = (0.5)*(Eamp**2/Z1)*(((3*np.sqrt(3))/2)*((keys['pitch']*keys['uol'])**2)))*math.cos(math.radians(keys['AOI'])) # ((Eamp V/m)/ohms)*m^2 = Watts
+            #hexagon area
+            #p_in = 1.0 * (((3*np.sqrt(3))/2)*((keys['pitch']*keys['uol'])**2))*math.cos(math.radians(keys['AOI'])) # Eamp is not used because PowerScaling is being used in sources.jcmt
+
+            #square
+            #p_in = 1.0 * (((keys['pitch']*keys['uol'])**2))*math.cos(math.radians(keys['AOI'])) # Eamp is not used because PowerScaling is being used in sources.jcmt
+            p_in = 1.0 * (((keys['pitch']*keys['uol'])**2)) # Eamp is not used because PowerScaling is being used in sources.jcmt
+
+            absS_pillar = (np.real(np.sum(table['ElectromagneticFieldAbsorption'][0][n2]))/p_in)
+            absP_pillar = (np.real(np.sum(table['ElectromagneticFieldAbsorption'][1][n2]))/p_in)
+         
+            absS_film = (np.real(np.sum(table['ElectromagneticFieldAbsorption'][0][n3]))/p_in)
+            absP_film = (np.real(np.sum(table['ElectromagneticFieldAbsorption'][1][n3]))/p_in)
+            
+            absS_amino = (np.real(np.sum(table['ElectromagneticFieldAbsorption'][0][n4]))/p_in)
+            absP_amino = (np.real(np.sum(table['ElectromagneticFieldAbsorption'][1][n4]))/p_in)
+
+            # Gather Reflected Fourier Modes (Z)
+            filename_fourierModes_r = './project_results/fourier_modes_r.jcm';
+            fourierModes_r = jcmwave.loadtable(filename_fourierModes_r,format='named')
+            powerFlux_r = jcmwave.convert2powerflux(fourierModes_r)
+
+            # Reflected flux in normal direction
+            P_s_r = np.real(np.sum(powerFlux_r['PowerFluxDensity'][0][:, 2]));
+            P_p_r = np.real(np.sum(powerFlux_r['PowerFluxDensity'][1][:, 2])); 
+		
+            filename_MM = './project_results/sm.jcm'
+            table = jcmwave.loadtable(filename_MM)
+            rows = []
+            for num in range(1):
+                mm = []
+                for i in range(4):
+                    row = []
+                    for j in range(4):
+                        row.append(float(table['Mueller_xy'+str(1+i)+str(1+j)][0][1]))
+                        #row.append(float(table['Mueller_xy'+str(1+i)+str(1+j)][0]))
+                    mm.append(row)
+                rows.append(mm)
+
+            id_names = ['azimuth','AOI','wvl','radius','pitch','height', 'kappa']
+
+            set.append(Tag(id_names, [azi, aoi, wvl, keys['radius'], keys['pitch'], 50, kappa]), ["abs pillar", "abs film", "abs amino", "reflected flux", "mm"], [[absS_pillar, absP_pillar], [absS_film, absP_film], [absS_amino, absP_amino], [P_s_r, P_p_r], rows[0]])
+            #set.append(Tag(id_names, [azi, aoi, wvl, keys['radius'], keys['pitch'], 50, kappa]), ["abs film", "abs air", "reflected flux", "mm"], [[absR_film, absL_film], [absR_air, absL_air], [P_s_r, P_p_r], mm])
+            set.save_json(output_file)
+            exit()
+	    
+        toc = time.time() # use time() not clock() on linux system  
+        t = toc-tic
+        print ("Total runtime for WVL: "+str(wvl)+" AOI: "+str(aoi)+" aziumth: "+str(azi)+" - %6.4f s" % t)
+
