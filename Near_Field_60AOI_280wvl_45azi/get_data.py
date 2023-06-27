fd634298d3d4ca8adc795f9e6e4ed853219529f6
Colton Bruni <colton.bruni@gmail.com>
2023-06-27 04:51:45 -0500

    testing Y data csv

--- Near_Field_60AOI_280wvl_45azi/get_data.py
+++ Near_Field_60AOI_280wvl_45azi/get_data.py
@@ -2,18 +2,27 @@ import jcmwave
 import pandas as pd
 
 
+eleclow = jcmwave.loadcartesianfields('./project_results/ElectricChiralityDensity_xy_z10.jcm')
+maglow = jcmwave.loadcartesianfields('./project_results/MagneticChiralityDensity_xy_z10.jcm')
+#pd.DataFrame(eleclow['field'][0][:,:,0].real + maglow['field'][0][:,:,0].real).to_excel('C0low.xlsx')
+pd.DataFrame(eleclow['Y']).to_csv('eleclow_Y0.csv')
+#pd.DataFrame(eleclow['X']).to_excel('eleclow_X0.xlsx')
+#pd.DataFrame(eleclow['Z']).to_excel('eleclow_Z0.xlsx')
 
+exit()
+elechigh = pd.DataFrame(jcmwave.loadcartesianfields('./project_results/ElectricChiralityDensity_xy_z90.jcm')['field'])
+elechighZero = pd.DataFrame(elechigh[0])
+elechighOne = pd.DataFrame(elechigh[1])
 
- 
-eleclow = pd.DataFrame(jcmwave.loadcartesianfields('ElectricChirality_xy_z10.jcm')['field'])
+maglow = pd.DataFrame(jcmwave.loadcartesianfields('./project_results/MagneticChiralityDensity_xy_z10.jcm')['field'])
+maglowZero = pd.DataFrame(maglow[0])
+maglowOne = pd.DataFrame(maglow[1])
 
-elechigh = pd.DataFrame(jcmwave.loadcartesianfields('ElectricChiralityDensity_xy_z90.jcm')['field'])
+maghigh = pd.DataFrame(jcmwave.loadcartesianfields('./project_results/MagneticChiralityDensity_xy_z90.jcm')['field'])
+maghighZero = pd.DataFrame(maghigh[0])
+maghighOne = pd.DataFrame(maghigh[1])
 
-maglow = pd.DataFrame(jcmwave.loadcartesianfields('MagneticChirality_xy_z10.jcm')['field'])
-
-maghigh = pd.DataFrame(jcmwave.loadcartesianfields('MagneticChiralityDensity_xy_z90.jcm')['field'])
-
-densities = [['eleclow', eleclow], ['elechigh', elechigh], ['maglow', maglow], ['maghigh', maghigh]]
+densities = [['eleclowOne', eleclowOne], ['eleclowZero', eleclowZero], ['elechighZero', elechighZero], ['elechighOne', elechighOne], ['maglowZero', maglowZero],['maglowOne', maglowOne], ['maghighZero', maghighZero], ['maghighOne', maghighOne]]
 
 for density in densities:
     density[1].to_excel(density[0]+'.xlsx')
