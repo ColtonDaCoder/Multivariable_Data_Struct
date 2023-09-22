aec6d8a638cde19a4134c0dfe36197c3a452f50b
Corrosive <coltonbruni@Coltons-MacBook-Pro.local>
2023-09-22 16:22:49 -0500

    ready for post processing

--- nearfield_architect.py
+++ nearfield_architect.py
@@ -11,21 +11,40 @@ jcm_root = "C:\\Users\\Colton Bruni\\Documents\\JCMsuite"
 sys.path.append(os.path.join(jcm_root, 'ThirdPartySupport', 'Python'))
 import jcmwave
 os.chdir("Github/Multivariable_Data_Struct/")
-folder = "Near_Field_60AOI_batch_13_14/project_results_azi13"
-
-folder = "LH_Gammadion_Simulation_Racemic_Trp_500/project_results/"
-cfb = jcmwave.loadcartesianfields(folder+'_ElectricChiralityDensity_xy_z3.jcm')
-chi = cfb['field'][1]
-cfb = jcmwave.loadcartesianfields(folder+'_MagneticChiralityDensity_xy_z3.jcm')
-chim = cfb['field'][1]
 import matplotlib.pyplot as plt
-C1 = chi[:,:,0].real+chim[:,:,0].real
-fig, ax = plt.subplots(figsize=(8,4))
+folder = "Near_Field_60AOI_batch_13_14/project_results_azi"
+#folder = "LH_Gammadion_Simulation_Racemic_Trp_500/project_results/"
 
-plt.subplot(1,1,1)
-plot1 = plt.pcolormesh(cfb['X']*10**9, cfb['Y']*10**9, C1, cmap=plt.cm.seismic, shading='gouraud')
-cb = plt.colorbar(plot1)
-plt.show()
+#remember ----
+# - old azi/wvl
+# - new /wvl
+# - new /trans_wvl
+def ChiralDensityPlot(folder, trans_wvl, heights, azis):
+    for azi in azis:
+        for wvl in trans_wvl:
+            plug = folder+azi+'/'+trans_wvl
+            for h in heights:
+                cfb = jcmwave.loadcartesianfields(plug+'_ElectricChiralityDensity_xy_z'+h+'.jcm')
+                chi = cfb['field'][1]
+                cfb = jcmwave.loadcartesianfields(plug+'_MagneticChiralityDensity_xy_z'+h+'.jcm')
+                chim = cfb['field'][1]
+                C1 = chi[:,:,0].real+chim[:,:,0].real
+                fig, ax = plt.subplots(figsize=(8,4))
+                plt.subplot(1,1,1)
+                plot1 = plt.pcolormesh(cfb['X']*10**9, cfb['Y']*10**9, C1, cmap=plt.cm.seismic, shading='gouraud')
+                cb = plt.colorbar(plot1)
+                plt.show()
+azis = [str(13 + i) for i in range(8)]
+wvl = [212 + i for i in range(14)]
+heights = ['56', '104', '120']
+s_wvl = []
+for l in lambdas:
+   s_wvl.append('wvl' + str(l))
+ChiralDensityPlot(folder, s_wvl, heights, azis) 
+exit()
+
+
+
 folder = "LH_Gammadion_Simulation_Racemic_Trp_500/project_results/"
 cfb = jcmwave.loadcartesianfields(folder+'_ElectricChiralityDensity_xy_z90.jcm')
 chi = cfb['field'][1]
