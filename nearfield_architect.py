3cde01fef7ce7982f3cfd2dd808f0360ab47bddd
Corrosive <colton.bruni@gmail.com>
2023-09-26 23:33:40 -0700

    gammadian azi trans normalized

--- nearfield_architect.py
+++ nearfield_architect.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy.ndimage import median_filter
 folder = "Near_Field_60AOI_batch_13_14/project_results_azi"
-#folder = "LH_Gammadion_Simulation_Racemic_Trp_500/project_results/"
+folder = "LH_Gammadion_Simulation_Racemic_Trp_500/project_results/"
 
 #remember ----
 # - old azi/wvl
@@ -35,19 +35,19 @@ def ChiralDensityPlot(folder, trans_wvl, heights, azis):
                 if first:
                    C1 = C0
                    first = False
-                fig, ax = plt.subplots(figsize=(8,4))
-                plt.subplot(1,1,1)
             data = C1/C0
             fdata = scipy.ndimage.median_filter(data,size=(4,4))
+            fig, ax = plt.subplots(figsize=(8,4))
+            plt.subplot(1,1,1)
             plot1 = plt.pcolormesh(cfb['X']*10**9, cfb['Y']*10**9, C1/C0, cmap=plt.cm.seismic, shading='gouraud')
             cb = plt.colorbar(plot1)     
-            plt.savefig("Normalized Pillar Chiral Density/azi"+str(azi)+str(wvl)+"normalized.png")
-azis = [str(17 + i) for i in range(4)]
-lambdas = [225 + i for i in range(1)]
-heights = ['104', '120']
+            plt.savefig("gammadian azi"+str(azi)+str(wvl)+"realnormalized.png")
+azis = ['']
+lambdas = [200, 210]
+heights = ['125', '150']
 s_wvl = []
 for l in lambdas:
-   s_wvl.append('wvl' + str(l))
+   s_wvl.append('trans_wvl' + str(l))
 ChiralDensityPlot(folder, s_wvl, heights, azis) 
 exit()
 
